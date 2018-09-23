7e7036f88196f131985bc7889c31b0ab5bc9167d
Chirag7077 <chiragb99@gmail.com>
2018-09-23 19:29:52 +0530

    Login

--- assettrackingsystem/__init__.py
+++ assettrackingsystem/__init__.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'db1bacd0b582a7e7427d98054e3d3517'
+
 from assettrackingsystem import routes
--- assettrackingsystem/routes.py
+++ assettrackingsystem/routes.py
@@ -1,7 +1,14 @@
 from assettrackingsystem import app
 from flask import render_template, url_for, flash
+from assettrackingsystem.forms import LoginForm
 
 @app.route("/")
 @app.route("/home")		
 def home():
     return render_template("index.html",title="Home Page")
+
+
+@app.route("/login",methods=['GET','POST'])
+def login():
+	form = LoginForm()
+	return render_template('login.html', title="Login", form=form)
