8571d94cc2f953cfe51a1a64e1276203102bc915
jslimaye <2016.jalaj.limaye@ves.ac.in>
2018-09-24 01:29:51 +0530

    Gitacora saved 1 changed files

--- assettrackingsystem/__init__.py
+++ assettrackingsystem/__init__.py
@@ -4,4 +4,4 @@ from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'db1bacd0b582a7e7427d98054e3d3517'
 
-from assettrackingsystem import routes
+from assettrackingsystem import routes,functions
