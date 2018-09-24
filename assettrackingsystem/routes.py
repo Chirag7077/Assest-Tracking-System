f5b47a2c70fd0a46f5458989a64a11e691c63802
meghasahu <megha.suresh.sahu@gmail.com>
2018-09-24 12:19:55 +0530

    changes in routes added admin pages

--- assettrackingsystem/routes.py
+++ assettrackingsystem/routes.py
@@ -1,6 +1,6 @@
 from assettrackingsystem import app
 from assettrackingsystem import bcrypt
-from flask import render_template, url_for, flash, redirect, session
+from flask import render_template, url_for, flash, redirect
 from assettrackingsystem.forms import LoginForm
 from assettrackingsystem.functions import authenticate
 from flask_login import login_user, current_user, logout_user, login_required
@@ -19,19 +19,33 @@ def login():
 		email = form.email.data
 		password = form.password.data
 		#hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-		status, roles, user_id = authenticate(email,password)
+		status, roles = authenticate(email,password)
 		if status == 1:
-			session['user_id'] = email
-			session['roles'] = roles
-			return redirect(url_for('dashboard')), session
+			login_user(user, remember=form.remember.data)
+			return redirect(url_for('home'))
 		else:
 			flash("Login Unsucessful. Please try again.","danger")
 	
 	return render_template('login.html', title="Login", form=form)
 
+@app.route("/admin")
+def admin():
+	return render_template("admin_layout.html",title="Welcome Admin")
+
+@app.route("/admin_add_user")
+def admin_add_user():
+	return render_template("admin_add_user.html",title="admin add")
+
+
+@app.route("/admin_modify_user")
+def admin_modify_user():
+	return render_template("admin_modify_user.html",title="admin add")
+
+
+@app.route("/admin_delete_user")
+def admin_delete_user():
+	return render_template("admin_delete_user.html",title="admin add")
+
 @app.route("/dashboard")
 def dashboard():
-	if session:
-		return render_template("Dash_template.html", title="Dashboard")
-	else:
-		return render_template("Dash_template.html", title="Dashboard no session")
+	return render_template("Dash_template.html", title="Dashboard")
